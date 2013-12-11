05af8df675d5e2e8d837e0c96929a03aed0fa00b
shak <sovannara.hak@isir.upmc.fr>
2013-12-11 18:07:13 +0100

    Add include directories defined in $CPLUS_INCLUDE_PATH in swig argument

--- setup.py
+++ setup.py
@@ -10,6 +10,13 @@ import commands
 
 flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
 
+def get_additional_include_dir_from_env():
+    kw = {'include_dirs':[], 'library_dirs':[], 'libraries':[]}
+    for token in commands.getoutput("echo $CPLUS_INCLUDE_PATH").split(":"):
+        if token != "":
+            kw['include_dirs'].append(token)
+    return kw
+
 # define pkgconfig return data
 def pkgconfig(pkgname, required=True):
 
@@ -52,18 +59,19 @@ def get_packages_data(lpkg):
 
 
 # try to find interesting module
-eigen3                 = pkgconfig("eigen3", True)
+eigen_lgsm             = pkgconfig("eigen_lgsm", True)
 quadprog               = pkgconfig("quadprog", True)
 orc_framework          = pkgconfig("orc_framework", True)
 orcisir_ISIRController = pkgconfig("orcisir_ISIRController", True)
 xdecore                = pkgconfig("xdecore", False)
+additional             = get_additional_include_dir_from_env()
 
 orocos                 = pkgconfig("orocos-rtt-gnulinux", False)
 
 # gather all data
 other_swig_opt = []
 other_compiler_args = []
-packages_data = get_packages_data([eigen3, quadprog, orc_framework, orcisir_ISIRController])
+packages_data = get_packages_data([eigen_lgsm, quadprog, orc_framework, orcisir_ISIRController, additional])
 
 
 # check for optional package: xde
